40f27e5d9b6cc6fa524bffd0b1ab697fab15a87f
mmahbub09 <meemmahbub09@gmail.com>
2024-12-10 20:56:20 -0500

    new update

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello Github! This is another update");
+
+        System.out.println("Hello everyone! Checking another sentence for github");
+
     }
 }
